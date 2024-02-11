f7000455ab869c0387310566f5cfa87065ed554e
Beaprofessor <130960371+Beaprofessor@users.noreply.github.com>
2024-02-11 10:05:21 +0000

    Datatypes

---
+++ JS/01_basics/02_dataTypes.js
@@ -0,0 +1,42 @@
+"use strict"; // treat all JS Code as newer version (ye engine ko btata hai ki new version mei code kar rhe hai)
+
+// Javscript ka engine browser mei chipa hua hai 
+
+// alert(3 + 3)    // there is different syntax to use alert.. && we are using nodejs , not browser
+
+console.log(3 + 3); console.log("Lokesh")  // code readability = 0
+
+console.log(3 + 3)    // code readability = 10/10
+console.log("Lokesh")
+
+// Refer **MDN** for documentation for standard JS
+//  Refer **tc39.es** for documentation for standard JS
+
+
+let name = "Lokesh"
+let age = 18
+let isLoggedIn = true 
+let state;
+
+
+// * Primitive DataTypes* 
+
+// number => 2 to power 53
+// bigInt => to use bahut bada number for  websites like trading..
+// string => ""
+// boolean => true/false
+// null => stand alone value hai  , representation of empty value , assign bhi kar sakte hai like null....
+// undefined => jab value assign  naa kari ho  , variable declare kardiya lekin value ni diya hai 
+// symbol => uniqueness find karne ke liye kaam aata hai 
+
+
+//  * Object *
+
+console.log(typeof null); // => object
+console.log(typeof undefined); // => undefined
+
+
+
+/*
+  Assignment => tc.39 par jaakar atleast specification jo jaakar dekhiye....
+ */
