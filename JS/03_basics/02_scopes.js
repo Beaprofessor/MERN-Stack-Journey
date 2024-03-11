9efa0190b3dd82ebc50d4331101dc566bc7d711f
Beaprofessor <professornberlin2122@gmail.com>
2024-03-11 12:57:43 +0530

    Scopes

---
+++ JS/03_basics/02_scopes.js
@@ -0,0 +1,17 @@
+// {} // isse hi scope bolte hai
+
+let a = 400
+
+if (true) {
+  let a = 10;
+  const b = 20;
+console.log("INNER:",a);
+   
+}
+
+// if ke andar hai block scope and iske bahar hai globle scope       
+
+
+console.log(a);
+// console.log(b);
+// console.log(c);
