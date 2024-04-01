60829a1eb6ac386d44eb16d50a97bb28d1ee6538
Beaprofessor <professornberlin2122@gmail.com>
2024-04-01 22:52:01 +0530

    Control Flow

---
+++ JS/04_control_flow/one.js
@@ -0,0 +1,31 @@
+// if 
+const isUserLoggedIn = true
+const temperature = 41
+
+
+// if(temperature < 50){
+//     console.log("less than 50");
+// }
+// else{
+//     console.log("temperature is greater than 50");
+// }
+
+// if ( 2 != 3){
+//    console.log("executed");
+// }
+
+// < , > , <= , >= , == , !=
+
+// if (false){
+
+//     // line no.7 se lekar bracket close tak code execute toh  nai hoga 
+// }
+
+
+const score = 200 
+if(score > 100){
+    const power = "fly"
+    console.log(`User power : ${power}`);
+}
+
+console.log(`User power : ${power}`);
