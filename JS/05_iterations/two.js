e7b78e21523b6a91c4dfe1a5345f94d612407c67
Beaprofessor <professornberlin2122@gmail.com>
2024-04-08 13:20:49 +0530

    while and do while loop

---
+++ JS/05_iterations/two.js
@@ -0,0 +1,31 @@
+// Syntax of the while loop
+// while (condition) {
+
+// }
+
+// let i = 0;
+// while (i < 10){
+// //  console.log(`Values of i is ${i}`);
+//  i=i+2;
+// }
+
+
+//  Array with the while loop
+
+let myArray = ["flash", "superman", "batman", "shaktimaan"];
+// console.log(myArray.length);
+let i = 0;
+while (i < myArray.length) {
+//   console.log(`Values of i is ${myArray[i]}`);
+  i = i + 1;
+}
+
+
+//  Do while loop 
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <= 10);
